8fa6a60fb9a08648e556675b3c091ef78b0b4dc8
ananyasaigal001 <50635283+ananyasaigal001@users.noreply.github.com>
2020-04-06 22:29:38 -0400

    Create Cylinder_Volume.py

---
+++ Cylinder_Volume.py
@@ -0,0 +1,7 @@
+import math
+pi= math.pi
+circle_radius=float(input("Enter the radius:"))
+height=float(input("Enter the height:"))
+area= pi*(circle_radius)**2
+volume=round((area*height), 1)
+print(volume)

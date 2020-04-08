e794ffcf04a7217cc17e8187e2106d45943d941c
ananyasaigal001 <50635283+ananyasaigal001@users.noreply.github.com>
2020-04-08 13:47:08 -0400

    Update Area_Of_A_Field.py

--- Area_Of_A_Field.py
+++ Area_Of_A_Field.py
@@ -1,5 +1,10 @@
-length = int(input("Enter your field's length in feet: "))
-width=int(input("Enter your field's width in feet: "))
+#obtaining length and width
+length = input("Enter your field's length in feet: "))
+width=input("Enter your field's width in feet: "))
+#converting length and width into int
+length=int(length)
+width=int(width)
+#finding the area
 area_one= length*width
 area_final=area_one//43560
 print("The area is",area_final,"in acres")

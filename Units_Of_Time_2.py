7031e3027030a3127c50f6f11cb339773ecd3faa
ananyasaigal001 <50635283+ananyasaigal001@users.noreply.github.com>
2020-04-08 14:19:50 -0400

    Update Units_Of_Time_2.py

--- Units_Of_Time_2.py
+++ Units_Of_Time_2.py
@@ -1,5 +1,7 @@
-time_seconds=int(input("Enter the time in seconds: "))
-
+#obtaining seconds
+time_seconds=input("Enter the time in seconds: ")
+time_seconds=int(time_seconds)
+#computing the days,hours,minutes,and seconds
 days="{0:0=2d}".format(time_seconds//86400)
 
 hours="{0:0=2d}".format((time_seconds% 86400)//3600)
@@ -10,4 +12,5 @@ minutes="{0:0=2d}".format(time_remaining//60)
 
 seconds="{0:0=2d}".format(time_remaining%60)
 
+#output
 print(days,":",hours,":",minutes,":",seconds)

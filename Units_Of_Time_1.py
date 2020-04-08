8c8a02cc12dac346a91070dbde7a8e346a129eee
ananyasaigal001 <50635283+ananyasaigal001@users.noreply.github.com>
2020-04-08 14:13:53 -0400

    Update Units_Of_Time_1.py

--- Units_Of_Time_1.py
+++ Units_Of_Time_1.py
@@ -1,7 +1,9 @@
+#obtaining days,hours,minutes,seconds
 days=int(input("Enter the number of days: "))
 hours=int(input("Enter the number of hours: "))
 minutes=int(input("Enter the amount of minutes: "))
 seconds=int(input("Enter the amount of seconds: "))
+#obtaining the seconds
 time_seconds=((days*86400)+(hours*3600)+(minutes*60)+seconds)
-
+#output
 print(time_seconds)     
